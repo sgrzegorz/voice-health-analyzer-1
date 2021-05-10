29a99a1a1ab8dee9e3cbb580ef40a8611fe518e1
pulnara <magnes.agnieszka@gmail.com>
2021-05-11 00:28:25 +0200

    Add buttons to record and play voice in app

--- app/src/main/java/pl/edu/agh/im/voicehealthanalyzer/MainActivity.kt
+++ app/src/main/java/pl/edu/agh/im/voicehealthanalyzer/MainActivity.kt
@@ -1,11 +1,80 @@
 package pl.edu.agh.im.voicehealthanalyzer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaRecorder: MediaRecorder? = null
+    private var mediaPlayer: MediaPlayer? = null
+
+    private var audioFilePath: String? = null
+    private lateinit var stopButton: Button
+    private lateinit var playButton: Button
+    private lateinit var recordButton: Button
+
+    private var isRecording = false
+
+    private val permissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recordButton = findViewById(R.id.recordButton)
+        playButton = findViewById(R.id.playButton)
+        stopButton = findViewById(R.id.stopButton)
+
+        if (!hasMicrophone()) {
+            stopButton.isEnabled = false
+            playButton.isEnabled = false
+            recordButton.isEnabled = false
+        } else {
+            playButton.isEnabled = false
+            stopButton.isEnabled = false
+        }
+
+        audioFilePath = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
+
+        if (!hasNecessaryPermissions()) {
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        }
+    }
+
+    private fun hasNecessaryPermissions(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    protected fun hasMicrophone(): Boolean {
+        val pmanager = this.packageManager
+        return pmanager.hasSystemFeature(
+            PackageManager.FEATURE_MICROPHONE
+        )
     }
 }
